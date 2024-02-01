2b88f0c757b3ef3f6bc8f29107aef7fde8ced7a5
ayusshrathore <ayusshrathore@gmail.com>
2024-02-01 17:48:11 +0530

    chore: make select course dropdown editable

--- src/Screens/Createpackage.js
+++ src/Screens/Createpackage.js
@@ -839,7 +839,7 @@ const Createpackage = (params) => {
 					<select
 						onChange={onSelectCourse}
 						value={getEditModePacDetail.courseId}
-						disabled={getEditMode}
+						// disabled={getEditMode}
 						className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
 					>
 						<option value={""}>Please Select</option>
